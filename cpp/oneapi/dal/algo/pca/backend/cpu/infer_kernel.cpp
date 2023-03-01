3076331626380c206d9f163647875655103332ed
Solovev, Aleksandr <aleksandr.solovev@intel.com>
2023-03-01 09:55:35 +0100

    fix infer

--- cpp/oneapi/dal/algo/pca/backend/cpu/infer_kernel.cpp
+++ cpp/oneapi/dal/algo/pca/backend/cpu/infer_kernel.cpp
@@ -51,6 +51,8 @@ static result_t call_daal_kernel(const context_cpu& ctx,
 
     const auto daal_data = interop::convert_to_daal_table<Float>(data);
     const auto daal_eigenvectors = interop::convert_to_daal_table<Float>(model.get_eigenvectors());
+    const auto daal_means = interop::convert_to_daal_table<Float>(model.get_means());
+    const auto daal_eigenvalues = interop::convert_to_daal_table<Float>(model.get_eigenvalues());     
     const auto daal_result =
         interop::convert_to_daal_homogen_table(arr_result, row_count, component_count);
 
@@ -58,9 +60,9 @@ static result_t call_daal_kernel(const context_cpu& ctx,
         interop::call_daal_kernel<Float, daal_pca_transform_kernel_t>(ctx,
                                                                       *daal_data,
                                                                       *daal_eigenvectors,
+                                                                      *daal_means,
                                                                       nullptr,
-                                                                      nullptr,
-                                                                      nullptr,
+                                                                      *daal_eigenvalues,
                                                                       *daal_result));
 
     return result_t{}.set_transformed_data(
